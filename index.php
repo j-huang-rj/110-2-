af9e2da9cc4f8fc7786d890494d3660b3c7f0ab5
j-huang-rj <100328343+j-huang-rj@users.noreply.github.com>
2022-03-13 11:47:26 +0800

    HW - Week 3 - 墾丁三日遊

--- index.php
+++ index.php
@@ -1,19 +1,63 @@
 <html>
-<head>
 
+<head>
+<title>墾丁三日遊</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="shortcut icon" href="https://www.pngitem.com/pimgs/m/62-622689_multi-colors-in-circle-png-image-colorful-circle.png" type="image/x-icon" />
+<link rel="icon" href="https://www.pngitem.com/pimgs/m/62-622689_multi-colors-in-circle-png-image-colorful-circle.png" type="image/x-icon" />
+<link rel="bookmark" href="https://www.pngitem.com/pimgs/m/62-622689_multi-colors-in-circle-png-image-colorful-circle.png" type="image/x-icon" />
 </head>
 
-<body>
-<?php
+<body bgcolor="#ECE7C3">
 
-$i = 10;
-$j = 20;
-echo "<b>Hello</b> <h1>PHP</h1>";
-echo "<br/>";
-echo $i + $j;
-echo "<br/>";
-echo date("Y.m.d");
-?>
+<h2><center><b><span style = "font-size:36px;">墾丁三日遊</span></b></center><h2>
+<hr color = "#606060" width = "100%"/>
+
+<ul>
+<h2><span style = "font-size:24px"><b><li>行程內容</li></b></span><h2>
+
+<p style = "font-size:18px">天空正美、海水正藍，春末夏初是墾丁最宜人的時節。</br>
+一起恣意地享受國境之南的陽光與大海，跟著我們來去墾丁吧!</p>
+
+<p>
+<a href = "http://uukt.com.tw/attraction/21" target = "new"><img src = "https://www.northguan-nsa.gov.tw/att/pic/z_11003055.jpg" width = 40%/></a>
+<a href = "https://ljl.uukt.com.tw/" target = "new"><img src = "https://img.ltn.com.tw/Upload/news/600/2020/08/05/119.jpg" width = 40%/></a>
+</p>
+
+<h2 style = "font-size:24px"><b><li>欲參加，請填寫以下表單：</li></b><h2>
+
+<form action = "" method = "">
+<span style = "font-size:18px">
+<p>姓名：<input type = "text" name = "name" placeholder = "請輸入" required/></span></p>
+<p>性別：<input type = "radio" name = "gender" required/>男
+<input type = "radio" name = "gender" required/>女
+<input type = "radio" name = "gender" required/>以上皆非</p>
+<p>生日：<input type = "date" name = "birthday" required/></p>
+<p>電話：<input type = "text" name = "telnum" placeholder = "請輸入" required/></p>
+<p>Email：<input type = "text" name = "email" placeholder = "請輸入" required/></p>    
+<p>飲食偏好 (多選)：<input type = "checkbox" name = venison>鹿肉
+<input type = "checkbox" name = beef>牛肉
+<input type = "checkbox" name = veg>豬肉</p>
+<p>活動T恤尺寸：
+<select name = "t-size">
+<option>S</option>
+<option>M</option>
+<option>L</option>
+<option>XL</option>
+</select></p>
+<p><table border = '2' width = '400' height = '200'>
+<tr><th></th><th>S</th><th>M</th><th>L</th><th>XL</th><tr>
+<tr><th>胸圍</th><th>41</th><th>43</th><th>45</th><th>48</th><tr>
+<tr><th>肩寬</th><th>98</th><th>104</th><th>112</th><th>120</th><tr>
+<tr><th>衣長</th><th>66</th><th>68</th><th>72</th><th>76</th><tr>
+</table></p>
+<p>活動T恤顏色(紅/藍)：<input type = "text" name = "t-color" placeholder = "請輸入" required/></p>
+<p>活動票：<input type = "number" name = "ticket" placeholder = "請選擇" required>張</p>
+<input type = "submit"/>
+<input type = "reset"/>
+</span>
+</form>
+</ul>
 </body>
 
 </html>
